ab11d631e7817ea030420a359480d7049fc46bf2
Dennis Marchuk <dvmarchuk4@gmail.com>
2020-12-11 01:49:37 -0500

    Updated format

---
+++ HW2/template1.js
@@ -0,0 +1,15 @@
+// -Take the template1.html file, attach a script to it, and work in it.
+//
+// - Write code that uses document.getElementById or document.getElementsByClassName or document.getElementsByTagName:
+// - gets the text from the paragraph with id "content"
+// - gets the text from the block with id "rules"
+// - replace the text of the paragraph with id 'content' with any other
+// - replace the text of the paragraph with id 'rules' with any other
+// - Change the background color of each element to red
+// - Change the color of each element to blue
+// - get the entire list of element classes with id = rules and display them in console.log
+// - get all elements with class fc_rules
+// - change the color of the text in all elements of fc_rules to red
+
+let contents = document.getElementById("content").innerHTML;
+console.log(contents)
